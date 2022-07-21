5a6b0291a4977aebe813ce395c0e60866daf610e
Anastasia <bonnyn7@yandex.ru>
2022-07-21 17:52:17 +0300

    Added hw7

---
+++ Task2/Task2.cs
@@ -0,0 +1,79 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+using System;
+
+namespace Homework7
+{
+    class Task2
+    {
+        static void RandomArray (int [,] array, int m, int n)
+        {
+            Random random = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(10);
+                    Console.Write(array[i, j]+"");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PrintArray(int[,]array,int m,int n)
+        {
+            for (int rows=0; rows< m; rows++)
+            {
+                for (int columns=0;columns<n; columns++)
+                {
+                    Console.WriteLine($"{array[rows,columns]}");
+                }
+            }
+
+        }
+        static bool CheckElement(int [,] array, int a, int b)
+        {
+            if (a > -1 && a <= array.GetUpperBound(0) && 
+                b > -1 && b <= array.GetUpperBound(1)) 
+            return true;
+            return false;
+        }
+        static void Solution (int[,]array, int a, int b)
+        {
+            if (CheckElement(a,b))
+            {
+                Console.WriteLine("Значение элемента [{0},{1}] = {2}", a, b, array[a,b]);
+            }
+            else
+            {
+                Console.WriteLine("Элементы [{0},{1}] не найдены", a, b);
+            }
+
+        }
+        static void Main (string [] args)
+        {
+            Console.Write("Введите m: ");
+            int m=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите n: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+           
+            int [,]array=new int[m,n];
+
+            Console.WriteLine("Введите индекс #1 элемента массива:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите индекс #2 элемента массива:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            RandomArray(array,m,n);
+            PrintArray(array, m, n);
+            CheckElement(array,a,b);
+            Solution(array,a,b);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task3/Task3.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+namespace Homework7
+{
+    class Task3
+    {
+        static int[,] array = {{3,2,7,1}, {4,2,8,3}, {9,1,6,2}};
+        static float[] arrayAverage = new float[array.GetUpperBound(1) + 1];
+        static void FindAverageOfSecondArray()
+        {
+            for (int j = 0; j <= array.GetUpperBound(1); j++)
+            {
+                int i;
+                for (i = 0; i <= array.GetUpperBound(0); i++)
+                {
+                    arrayAverage[j] += array[i,j];
+                }
+                arrayAverage[j] = arrayAverage[j] / (i);
+            }
+        }
+        static void ViewAverage()
+        {
+            Console.Write("Среднее арифметическое каждого столбца: ");
+            for (int i = 0; i < arrayAverage.Length; i++)
+            {
+                Console.Write("{0}; ", Math.Round(arrayAverage[i], 1));
+            }
+        }
+        static void Main(string[] args)
+        {
+            FindAverageOfSecondArray();
+            ViewAverage();
+        }
+    }
+}
